dbbcbf36887be7d4670d592221c88ae64dabb100
olofWallgren <olof.wallgren@medieinstitutet.se>
2020-10-23 14:45:51 +0200

    add js-doc coments

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-//start function when button is clicked
+/**
+ * Will generate a random number and pprint it into the DOM
+ */
 function printRandomNumber(){
 const randomNumber = getRandomNumber();
 addNumberToPage(randomNumber);
 }
-// Help function
+/**
+ * Generates a random number and returns it
+ * @returns {number} the random number that was generated
+ */
 function getRandomNumber(){
     const randomNumber = Math.random() * 100;
     return Math.round(randomNumber);
 }
-//helper function
+/**
+ * Places the passed in number into the DOM along with a comma and space
+ * @param {number} number 
+ */
 function addNumberToPage(number){
     const div = document.getElementById('box')
     div.append(number + ', ');
